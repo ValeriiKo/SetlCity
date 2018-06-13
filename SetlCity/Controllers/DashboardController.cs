74a0e54875fb00fb2c2c97fbaa8aafa28a2bc032
Valerii <valeriikozhevnikov@gmail.com>
2018-06-13 21:31:15 +0300

    Authentification

---
+++ SetlCity/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using SetlCity.Models;
+
+namespace SetlCity.Controllers
+{
+    [CheckAuthorization]
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SetlCity/Controllers/HomeController.cs
@@ -0,0 +1,168 @@
+﻿using System.Linq;
+using System.Security.Principal;
+using System.Web.Mvc;
+using System.Web.Security;
+using SetlCity.Models;
+
+namespace SetlCity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private SetlCityDBEntities db = new SetlCityDBEntities();
+        //todo get saltLengthLimit from config
+        private static int saltLengthLimit = 32;
+
+        #region --> Login GET Method 
+
+        [HttpGet]
+        public ActionResult Login(string returnURL)
+        {
+            var userinfo = new LoginVM();
+
+            try
+            {
+                // We do not want to use any existing identity information
+                EnsureLoggedOut();
+
+                // Store the originating URL so we can attach it to a form field
+                userinfo.ReturnURL = returnURL;
+
+                return View(userinfo);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        #endregion
+
+        #region --> Pre Required Method For Login
+
+        //GET: EnsureLoggedOut
+        private void EnsureLoggedOut()
+        {
+            // If the request is (still) marked as authenticated we send the user to the logout action
+            if (Request.IsAuthenticated)
+                Logout();
+        }
+
+        //POST: Logout
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Logout()
+        {
+            try
+            {
+                // First we clean the authentication ticket like always
+                //required NameSpace: using System.Web.Security;
+                FormsAuthentication.SignOut();
+
+                // Second we clear the principal to ensure the user does not retain any authentication
+                //required NameSpace: using System.Security.Principal;
+                HttpContext.User = new GenericPrincipal(new GenericIdentity(string.Empty), null);
+
+                Session.Clear();
+                System.Web.HttpContext.Current.Session.RemoveAll();
+
+                // Last we redirect to a controller/action that requires authentication to ensure a redirect takes place
+                // this clears the Request.IsAuthenticated flag since this triggers a new request
+                return RedirectToLocal();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //GET: RedirectToLocal
+        private ActionResult RedirectToLocal(string returnURL = "")
+        {
+            try
+            {
+                // If the return url starts with a slash "/" we assume it belongs to our site
+                // so we will redirect to this "action"
+                if (!string.IsNullOrWhiteSpace(returnURL) && Url.IsLocalUrl(returnURL))
+                    return Redirect(returnURL);
+
+                // If we cannot verify if the url is local to our host we redirect to a default location
+                return RedirectToAction("Index", "Dashboard");
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //GET: SignInAsync   
+        private void SignInRemember(string userName, bool isPersistent = false)
+        {
+            // Clear any lingering authencation data
+            FormsAuthentication.SignOut();
+
+            // Write the authentication cookie
+            FormsAuthentication.SetAuthCookie(userName, isPersistent);
+        }
+
+        #endregion
+
+        #region --> Login POST Method
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginVM entity)
+        {
+            string OldHASHValue = string.Empty;
+            byte[] SALT = new byte[saltLengthLimit];
+
+            try
+            {
+                using (db = new SetlCityDBEntities())
+                {
+                    // Ensure we have a valid viewModel to work with
+                    if (!ModelState.IsValid)
+                        return View(entity);
+
+                    //Retrive Stored HASH Value From Database According To Username (one unique field)
+                    var userInfo = db.Users.Where(s => s.Name == entity.Username.Trim()).FirstOrDefault();
+
+                    //Assign HASH Value
+                    if (userInfo != null)
+                    {
+                        OldHASHValue = userInfo.Hash;
+                        SALT = userInfo.Salt;
+                    }
+
+                    bool isLogin = Hashing.CompareHashValue(entity.Password, entity.Username, OldHASHValue, SALT);
+
+                    if (isLogin)
+                    {
+                        //Login Success
+                        //For Set Authentication in Cookie (Remeber ME Option)
+                        SignInRemember(entity.Username, entity.isRemember);
+
+                        //Set A Unique ID in session
+                        Session["UserID"] = userInfo.Id;
+
+                        // If we got this far, something failed, redisplay form
+                        // return RedirectToAction("Index", "Dashboard");
+                        return RedirectToLocal(entity.ReturnURL);
+                    }
+                    else
+                    {
+                        //Login Fail
+                        TempData["ErrorMSG"] = "Access Denied! Wrong Credential";
+                        return View(entity);
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+
+        #endregion
+    }
+}
